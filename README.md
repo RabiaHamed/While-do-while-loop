73b3f4d73f79fc48dd33c65587a0c87c5689d56a
Rabia Hameed <nrsuriya093@gmail.com>
2024-01-13 22:16:27 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# While-do-while-loop
---
+++ code.js
@@ -0,0 +1,30 @@
+//********************************Task 1************************************//
+
+// var num1 = 8;
+// var num2 = 4;
+//    function sum(a,b) {
+//     var calcu = Math.pow(a,b);
+//     return  calcu;
+//    }
+
+// var calcu = sum(num1,num2);
+
+// console.log("a" + ""+ num1 +  "value raised to b"  +  num2  +  "is:"  +  calcu);
+
+//********************************Task 2************************************//
+// function userYear(year) {
+//     switch (year %4 && year %100) {
+//         case 0:
+//             alert("It's Leap Year");
+//             break;
+//         default:
+//             alert("It's not leap year");
+//     }
+// }
+// // var userYear = prompt();
+// userYear(parseInt(prompt ("Enter a year")));
+
+//********************************Task 3************************************//
+
+
+
